9ad02d4da692c3a74ae62be5dd3ee8f1277af5a2
bsbagchi <brotisunder9@gmail.com>
2025-03-05 12:55:32 -0500

    refactor: remove GitHub Actions specific asset prefix and base path configuration

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,6 @@ try {
   // ignore error
 }
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
